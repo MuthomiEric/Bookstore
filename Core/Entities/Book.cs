346cbde2a0d4455ff7514d9779c25acea70842d8
Erick Muthomi <erick.njoka@icealion.com>
2022-02-06 11:36:29 +0300

    add entities

---
+++ Core/Entities/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+    public class Book : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime YearOfPublication { get; set; }
+        public virtual Guid AuthorId { get; set; }
+        public virtual Author Author { get; set; }
+    }
+}
